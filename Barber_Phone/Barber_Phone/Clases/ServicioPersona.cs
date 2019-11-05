f780f778d9e094800cddd24ba3471376798a195c
JeffxD0208 <55258246+JeffxD0208@users.noreply.github.com>
2019-11-05 00:07:49 -0400

    Se creo la clase ServicioPersona.cs, verAgendaBarbero, se modifico ver agenda barbero.xaml y cs, se modifico cliente.cs,, etc

---
+++ Barber_Phone/Barber_Phone/Clases/ServicioPersona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barber_Phone.Clases
+{
+    public class ServicioPersona
+    {
+
+        public List<Persona> ConsultarPersona()
+        {
+            var lista = new List<Persona>();
+
+            lista.Add(new Persona() { Nombre = "Jefferson", apellido = "Villanueva", hora = "10:15 AM" });
+            lista.Add(new Persona() { Nombre = "Josue", apellido = "Jimenez", hora = "11:15 AM" });
+            lista.Add(new Persona() { Nombre = "Benjamin", apellido = "Guchi", hora = "10:45 AM" });
+            return lista;
+        }
+
+
+
+
+
+
+    }
+}
--- Barber_Phone/Barber_Phone/Views/verAgendaBarbero.xaml.cs
+++ Barber_Phone/Barber_Phone/Views/verAgendaBarbero.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,7 +15,9 @@ namespace Barber_Phone.Views
     {
         public verAgendaBarbero()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            ObservableCollection<Cliente> listaPersona = new ObservableCollection<Cliente>(new ServicioPersona().ConsultarPersona());
+            lstPersonas.ItemsSource = listaPersonas;
         }
     }
 }
