58a1db2ccc4fccc36cef248f68e176a594a2d3e9
josue0142 <53450945+josue0142@users.noreply.github.com>
2019-11-09 20:57:32 -0400

    Minor bugs fix. Se crea el menu para el cliente

--- Barber_Phone/Barber_Phone/Views/opcionesMenuCliente.xaml.cs
+++ Barber_Phone/Barber_Phone/Views/opcionesMenuCliente.xaml.cs
@@ -21,12 +21,12 @@ namespace Barber_Phone.Views
                 await App.Modificador.Detail.Navigation.PushAsync(new verAgendaBarbero());
                 App.Modificador.IsPresented = false;
             };*/
-            /*Item2.Clicked += async (sender, e) =>
+            Item2.Clicked += async (sender, e) =>
             {
-                await App.Modificador.Detail.Navigation.PushAsync(new PantallaItemDos());
+                await App.Modificador.Detail.Navigation.PushAsync(new Crear_Cita());
                 App.Modificador.IsPresented = false;
             };
-            Item3.Clicked += async (sender, e) =>
+            /*Item3.Clicked += async (sender, e) =>
             {
                 await App.Modificador.Detail.Navigation.PushAsync(new PantallaItemTres());
                 App.Modificador.IsPresented = false;
