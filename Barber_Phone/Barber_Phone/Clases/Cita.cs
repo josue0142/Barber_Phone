d11c0bd5ce394f10c1f7cb6c1dedd8d5bc670916
josue0142 <53450945+josue0142@users.noreply.github.com>
2019-11-21 17:37:37 -0400

    Completado HistorialCliente

---
+++ Barber_Phone/Barber_Phone/Clases/Cita.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barber_Phone.Clases
+{
+    class Cita
+    {
+        private string cliente;
+        private string barbero;
+        private string barberia;
+        private string hora;
+        private string fecha;
+        private string servicio;
+        private string duracion;
+        private string estado;
+
+        public string Cliente { get => cliente; set => cliente = value; }
+        public string Barbero { get => barbero; set => barbero = value; }
+        public string Barberia { get => barberia; set => barberia = value; }
+        public string Hora { get => hora; set => hora = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Servicio { get => servicio; set => servicio = value; }
+        public string Duracion { get => duracion; set => duracion = value; }
+        public string Estado { get => estado; set => estado = value; }
+        
+    }
+}
